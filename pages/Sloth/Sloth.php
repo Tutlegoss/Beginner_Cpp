d59f3607759caa51b57a80d87e28301ba363631a
Tutlegoss <tutlegoss@aol.com>
2021-04-18 21:45:21 -0400

    Sloth Update

--- pages/Sloth/Sloth.php
+++ pages/Sloth/Sloth.php
@@ -21,9 +21,22 @@
             width: fit-content;
             width: -moz-fit-content;
             color: #08146eed;
-            font-size: 2.5rem;
+            font-size: 4rem;
             text-shadow: 0 4px 0 var(--wht), 0 4px 5px var(--wht), 0 4px 10px var(--wht);
         }
+        @media only screen and (max-width: 575.98px)
+        {
+            .title {
+                font-size: 2rem;
+            }
+        }
+        @media only screen and (min-width: 576px) and (max-width: 767.98px)
+        {
+            .title {
+                font-size: 3rem;
+            }
+        }
+
         .sources {
             font-family: 'Animal', monospace;
             color: #08146eed;
@@ -95,20 +108,20 @@
 		<div class="row mt-5 mb-4">
 		    <div class="col-12">
                 <div class="title mx-auto">
-                    <h2 class="text-center p-3 p-md-4">Megalonyx</h2>
+                    <p class="text-center m-0 p-5">Megalonyx</p>
                 </div>
                 <div class="author mx-auto">
                     <p>
                         <span class="kentYellow">Researched by</span>: Landen Marchand
                         <br>
-                        <span class="pl-5 kentYellow">Edited</span>: 17/Apr/2021
+                        <span class="pl-5 kentYellow">Edited</span>: 18/Apr/2021
                     </p>
                 </div>
             </div>
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
+            <div class="col-12 col-md-11 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
                 <h2 class="header pl-3">Introduction</h2>
         		<hr>
                 <p class="m-0">
@@ -123,7 +136,7 @@
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
+            <div class="col-12 col-md-11 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
                 <h2 class="header pl-3">Time Period</h2>
         		<hr>
                 <figure class="figLeft">
@@ -140,6 +153,10 @@
                     <a href="https://royalsocietypublishing.org/doi/10.1098/rspb.2013.3254">research</a>
                     that suggests that humans are to blame</a>, especially in North America where the climate was stable
                     and vegetation stayed relatively unchanged.
+                    <br>
+                    <br>
+                    Due to the extinction being relatively close to today, remains of the <em class="co-c">Megalonyx</em>
+                    can contain DNA that is not present in older fossils <span class="kentBlue">(Delsuc 2)</span>.
                 </p>
             </div>
         </div>
@@ -159,13 +176,13 @@
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
+            <div class="col-12 col-md-11 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
                 <h2 class="header pl-3">Habitat</h2>
         		<hr>
                 <figure class="figLeft">
                     <img src="./Living.png" data-toggle="modal" data-target="#Modal1">
                     <figcaption class="text-center">
-                        Fig. 1. <em>Megalonyx jeffersonii</em>: Ice Age Distribution. Grayson, Donald K., and Wally Woolfenden<em>
+                        Fig. 2. <em>Megalonyx jeffersonii</em>: Ice Age Distribution. Grayson, Donald K., and Wally Woolfenden<em>
                         Giant Sloths and Sabertooth Cats: Extinct Mammals and the Archaeology of the Ice Age Great Basin</em>.
                         University of Utah Press, 2016, p. 62.
                     </figcaption>
@@ -173,7 +190,7 @@
                 <p class="m-0">
                     The <em class="co-c">Megalonyx</em> lived all throughout North America. This differs from other sloths during this
                     time who mostly lived in specific North American regions. Therefore, it is the most wide-ranging of all
-                    sloths as remains have been found from Mexico to Alaska <span class="kentBlue">(Grayson)</span>. Figure 1 shows that most lived in the
+                    sloths as remains have been found from Mexico to Alaska <span class="kentBlue">(Grayson)</span>. Figure 2 shows that most lived in the
                     eastern United States with smaller concentrations on the west coast.
                     They resided in Ohio during the Pleistocene's warmer periods <span class="kentBlue">(Rocque 135)</span>.
                     <br>
@@ -201,7 +218,7 @@
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
+            <div class="col-12 col-md-11 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
                 <h2 class="header pl-3">Appearance</h2>
         		<hr>
                 <figure class="mx-auto mb-4" style="width: 90%">
@@ -211,7 +228,7 @@
                     </figcaption>
                 </figure>
                 <p class="pb-4">
-                    The <em class="co-c">Megalonyx</em> is of average size in terms of giant sloths coming in at most 9.8 feet long and up to 2,200 pounds. Figure 3 illustrates the height comparisons of each sloth (on average) and includes a human as a reference <span class="kentBlue">(Bradford)</span>. We see that the height is formidable due
+                    The <em class="co-c">Megalonyx</em> is of average size in terms of giant sloths coming in at about 9.8 feet long<sup class="co-r">*</sup> and up to 2,200 pounds<sup class="co-r">*</sup>. Figure 3 illustrates the height comparisons of each sloth (on average) and includes a human as a reference <span class="kentBlue">(Bradford)</span>. We see that the height is formidable due
                     to the sloths ability to stand on their flat-footed hind legs.
                     <br>
                     <br>
@@ -223,6 +240,10 @@
                     <br>
                     <br>
                     They were covered in fur for environmental protection.
+                    <br>
+                    <br>
+                    <span class="co-r">*</span>Every source lists different values for length and weight. There is no particular
+                    reason why I choose Bradford's data.
                 </p>
                 <div class="row">
                     <div class="col-12 pb-4 pb-md-0 col-md-6">
@@ -237,7 +258,7 @@
                         <figure class="figNormal">
                             <img src="./ZooSloth.jpg" data-toggle="modal" data-target="#Modal5">
                             <figcaption class="text-center">
-                                Fig. 5. <em>Megalonyx skeleton</em>. Katy, <a href="https://kccd.wordpress.com/2013/08/19/oops-i-ignored-july-part-two-ui-natural-history-museum/"><em>Giant Ground Sloth</em></a>. <a href="https://mnh.uiowa.edu/ice-age-giant-ground-sloth">University of Iowa Museum of Natural History</a>, 19 August 2013.
+                                Fig. 5. <em>Megalonyx</em>. Katy, <a href="https://kccd.wordpress.com/2013/08/19/oops-i-ignored-july-part-two-ui-natural-history-museum/"><em>Giant Ground Sloth</em></a>. <a href="https://mnh.uiowa.edu/ice-age-giant-ground-sloth">University of Iowa Museum of Natural History</a>, 19 August 2013.
                             </figcaption>
                         </figure>
                     </div>
@@ -273,22 +294,55 @@
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
-                <h2 class="header pl-3">Life and Diet</h2>
+            <div class="col-12 col-md-11 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
+                <h2 class="header pl-3">Life/Diet/Evolution</h2>
         		<hr>
-                <figure class="figLeft">
-                    <img src="./Living.png" data-toggle="modal" data-target="#Modal1">
+                <figure class="figLeft mb-0 mb-sm-5">
+                    <img src="./Coprolite.png" data-toggle="modal" data-target="#Modal6">
                     <figcaption class="text-center">
-
+                        Fig. 6. Lindsey, Emily L., et al. “A Monodominant Late-Pleistocene Megafauna Locality from Santa Elena, Ecuador: Insight on the Biology and Behavior of Giant Ground Sloths.” <em>Palaeogeography, Palaeoclimatology, Palaeoecology</em>, vol. 544, 15 April. 2020, p. 5., doi:10.1016/j.palaeo.2020.109599.
+                    </figcaption>
+                </figure>
+                <figure class="figLeft" style="clear:both">
+                    <img src="./Teeth.jpg" data-toggle="modal" data-target="#Modal7">
+                    <figcaption class="text-center">
+                        Fig 7. <em></em>. Mayhew <a href="https://www.cincinnati.com/story/news/local/boone-county/2017/09/08/science-looms-large-giant-ground-sloth-welcomed-big-bone/645879001/">Science looms large as Big Bone welcomes giant sloth</a>, <em>The Enquirer</em>, 8 September 2017.
                     </figcaption>
                 </figure>
                 <p class="m-0">
+                    The <em class="co-c">Megalonyx</em> is, in general, an augmented version of today's three-fingered sloths <span class="kentBlue">(Delsuc 4)</span>.
+                    <br>
+                    <br>
+                    They are said to be
+                    polyphyletic, which means they evolved from more than one common ancestor <span class="kentBlue">(Delsuc 4)</span>.
+                    <br>
+                    <br>
+                    They were
+                    herbivores, so their diet consisted of plants, foliage, leaves, etc. Figure 6 shows extracted plant
+                    material from sloth coprolite or its gut <span class="kentBlue">(Lindsey)</span>. Figure 7 shows
+                    the shape and structure of their teeth for grinding the plant life.
+                    <br>
+                    <br>
+                    Their hind feet and stout tail allowed them to stand and reach up trees for food <span class="kentBlue">(Lindsey)</span>.
+                    <br>
+                    <br>
+                    Their primary preditors would most likely be Saber-Toothed Cats and humans. It is believed that humans
+                    were the primary cause in their extinction  (<a href="https://royalsocietypublishing.org/doi/10.1098/rspb.2013.3254">further reading</a>).
+                    <br>
+                    <br>
+                    Their reproductive cycle is that of a regular mammal. Today's sloths give birth to one child with a gestation period ranging from <a href="https://sciencing.com/life-cycle-sloths-6758571.html">150 days to 11.5 months</a>. There is no information on the reproductive aspects on the <em class="co-c">Megalonyx</em>.
+                    <br>
+                    <br>
+                    Their lifespan may be about 20 years if you take this value from today's sloths. There appears to be
+                    no definitive answer.
+                    <br>
+                    <br>
 
                 </p>
             </div>
         </div>
 
-        <div class="modal fade" id="Modal1" tabindex="-1">
+        <div class="modal fade" id="Modal6" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content" style="background-color: var(--blk);">
                     <div class="modal-header">
@@ -296,32 +350,45 @@
                         <button type="button" class="btn btnBlue" data-dismiss="modal">Close</button>
                     </div>
                     <div class="modal-body">
-                        <img src="./Living.png" class="d-block w-100" alt="North American Distribution">
+                        <img src="./Coprolite.png" class="d-block w-100" alt="North American Distribution">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="Modal7" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content" style="background-color: var(--blk);">
+                    <div class="modal-header">
+                        <h5 class="modal-title kentYellow" id="exampleModalLabel"></h5>
+                        <button type="button" class="btn btnBlue" data-dismiss="modal">Close</button>
+                    </div>
+                    <div class="modal-body">
+                        <img src="./Teeth.jpg" class="d-block w-100" alt="North American Distribution">
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
+            <div class="col-12 col-md-11 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
                 <h2 class="header pl-3">Thomas Jefferson</h2>
         		<hr>
                 <figure class="figLeft">
-                    <img src="./FunnySloth.jpg" data-toggle="modal" data-target="#Modal6">
+                    <img src="./FunnySloth.jpg" data-toggle="modal" data-target="#Modal8">
                     <figcaption class="text-center">
-                        Fig. 6. <em>Megalonyx Jeffersonii Factoid</em>. CuriosityDaily_2015 <em>
+                        Fig. 8. <em>Megalonyx Jeffersonii Factoid</em>. CuriosityDaily_2015 <em>
                         This giant sloth was named after Thomas Jefferson...</em>.
                         <a href="https://ifunny.co/picture/www-ll-15-this-giant-sloth-was-named-after-thomas-el2AeAXZ4">iFunny post</a>, 8 February 2017.
                     </figcaption>
                 </figure>
                 <figure class="figLeft" style="clear: left">
-                    <img src="./JeffersonBones.jpg" data-toggle="modal" data-target="#Modal7">
+                    <img src="./JeffersonBones.jpg" data-toggle="modal" data-target="#Modal9">
                     <figcaption class="text-center">
-                        Fig. 7. <em>Megalonyx Claws</em>. <a href="https://en.wikipedia.org/wiki/Megalonyx"><em>Wikipedia Public Domain</em></a>.
+                        Fig. 9. <em>Megalonyx Claws</em>. <a href="https://en.wikipedia.org/wiki/Megalonyx"><em>Wikipedia Public Domain</em></a>.
                 </figure>
                 <p class="m-0">
                     What does the third president of the USA have anything to do with the <em class="co-c">Megalonyx</em>?
-                    Figure 6 is not your formal image for a field guide, but it conveys a little-known fact
+                    Figure 8 is not your formal image for a field guide, but it conveys a little-known fact
                     in a contemporary style.
                     <br>
                     <br>
@@ -350,7 +417,7 @@
             </div>
         </div>
 
-        <div class="modal fade" id="Modal6" tabindex="-1">
+        <div class="modal fade" id="Modal8" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content" style="background-color: var(--blk);">
                     <div class="modal-header">
@@ -363,7 +430,7 @@
                 </div>
             </div>
         </div>
-        <div class="modal fade" id="Modal7" tabindex="-1">
+        <div class="modal fade" id="Modal9" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content" style="background-color: var(--blk);">
                     <div class="modal-header">
@@ -378,11 +445,11 @@
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
+            <div class="col-12 col-md-11 col-lg-9 col-xl-7 pt-4 pb-4 slothRow">
                 <h2 class="header pl-3">In Ohio</h2>
         		<hr>
                 <figure class="figLeft">
-                    <img src="./OhioMap.gif" data-toggle="modal" data-target="#Modal8">
+                    <img src="./OhioMap.gif" data-toggle="modal" data-target="#Modal10">
                     <figcaption class="text-center">
 
                     </figcaption>
@@ -407,7 +474,7 @@
         </div>
 
 
-        <div class="modal fade" id="Modal8" tabindex="-1">
+        <div class="modal fade" id="Modal10" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content" style="background-color: var(--blk);">
                     <div class="modal-header">
@@ -422,7 +489,7 @@
         </div>
 
         <div class="row mb-4 justify-content-center">
-            <div class="col-12 col-md-10 col-lg-9 col-xl-8 pt-4 pb-4 slothRow" style="word-break: break-word;">
+            <div class="col-12 col-md-11 col-lg-9 col-xl-8 pt-4 pb-4 slothRow" style="word-break: break-word;">
                 <p class="sources text-center pt-2">Sources</p>
 
                 <p class="header pl-3">Books</p>
@@ -465,7 +532,7 @@
                     <svg data-icon="claw-marks" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 20px;">
                         <path fill="var(--lt-gold)" d="M6.99 224.51c-5-2.37-9.4 4.09-5.49 8l85.12 85.13c6 6 9.37 14.14 9.37 22.63V384h43.74c8.49 0 16.63 3.37 22.63 9.37l117.15 117.16c3.86 3.86 10.32-.56 7.99-5.49C206.4 333.11 63.36 251.26 6.99 224.51zM246.63 29.63c6 6 9.37 14.14 9.37 22.63V96h43.74c8.49 0 16.63 3.37 22.63 9.37l52.26 52.26c6 6 9.37 14.14 9.37 22.63V224h43.73c8.49 0 16.63 3.37 22.63 9.37l53.16 53.16c3.86 3.86 10.32-.56 7.99-5.49C430.4 109.11 287.36 27.26 230.99.51c-5-2.37-9.4 4.09-5.49 8l21.13 21.12zm-42.36 90.69c-7.8-6.05-12.27-15.15-12.27-25.03V64h-57.99c-6.01 0-12.04-1.84-17.12-5.05C74.85 32.39 38.37 13.23 12.42.91 3.54-3.3-4.28 8.18 2.67 15.13l179.96 179.96c6 6 9.37 14.14 9.37 22.63V256h38.29c8.49 0 16.62 3.37 22.63 9.37l89.71 89.71c6 6 9.37 14.14 9.37 22.63V416h38.29c8.49 0 16.62 3.37 22.63 9.37l84.01 84.01c6.86 6.86 18.34-.99 14.2-9.76-82.73-175.34-201.55-297.65-306.86-379.3z"></path>
                     </svg>
-                    Lindsey, Emily L., et al. “A Monodominant Late-Pleistocene Megafauna Locality from Santa Elena, Ecuador: Insight on the Biology and Behavior of Giant Ground Sloths.” Palaeogeography, Palaeoclimatology, Palaeoecology, vol. 544, 15 April. 2020, p. 109599., doi:10.1016/j.palaeo.2020.109599.
+                    Lindsey, Emily L., et al. “A Monodominant Late-Pleistocene Megafauna Locality from Santa Elena, Ecuador: Insight on the Biology and Behavior of Giant Ground Sloths.” <em>Palaeogeography, Palaeoclimatology, Palaeoecology</em>, vol. 544, 15 April. 2020, p. 109599., doi:10.1016/j.palaeo.2020.109599.
                 </p>
                 <p>
                     <svg data-icon="claw-marks" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 20px;">
